dd51035f706c2f8f40197d952b2559470c3ec8a0
Dina Hafez <dinahafez.8@gmail.com>
2018-06-29 14:43:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,30 +1,5 @@
-# solution with each 
-# def reverse_each_word(sentence) 
-#   reversed = []
-#   words = sentence.split
-#   words.each do |word|
-#     reversed << word.reverse
-#   end
-#   reversed.join(" ")
-# end
-
-# 1st solution with collect 
-# def reverse_each_word(sentence)
-#   reversed = []
-#   words = sentence.split
-#   words.collect do |word|
-#     reversed << word.reverse
-#   end
-#   reversed.join(" ")
-# end
-
-# refactored solution with collect
-# def reverse_each_word(sentence)
-#   reversed = []
-#   sentence.split.collect {|word| reversed << word.reverse}
-#   reversed.join(" ")
-# end
-
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  reversed = []
+  sentence.split.collect {|word| reversed << word.reverse}
+  reversed.join(" ")
 end
